4c38ef14dc602cc560db942b8cf5e0b8a1ef9d3e
Alby02 <albertomontrucchio02@gmail.com>
2024-05-24 16:41:27 +0200

    Spark

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    application
 }
 
 group = "it.uniupo.restfullMachineManager"
@@ -9,9 +9,14 @@ repositories {
     mavenCentral()
 }
 
+application{
+    mainClass = "it.uniupo.restfullMachineManager.Main"
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.sparkjava:spark-core:2.9.4")
 }
 
 tasks.test {
