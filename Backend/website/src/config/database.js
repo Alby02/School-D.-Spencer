78cb3a5317c15d2ced06339e9428b94809c98675
Alby02 <albertomontrucchio02@gmail.com>
2025-03-03 18:56:15 +0100

    fix mio errore docker

--- Backend/website/src/config/database.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('api', 'apiSpark', '123Pissir!', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
-
-module.exports = sequelize;
--- Backend/website/src/server.js
+++ Backend/website/src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const sequelize = require('./config/database');
 
 const app = express();
 
